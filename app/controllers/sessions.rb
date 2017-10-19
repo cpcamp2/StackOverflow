047adbd390e302ab74da12e00167ed2961cca71a
Chris Camp <cpcamp2@gmail.com>
2017-10-19 14:38:22 -0400

    Add routes/view files for user auth

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,21 @@
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+post '/sessions' do
+  user = User.authenticate(params[:user][:username], params[:user][:password])
+  if user
+    status 200
+    session[:user_id] = user.id
+    redirect '/suggestions'
+  else
+    status 422
+    @errors = ["Yeah, that's gonna be a no for me, dawg"]
+    erb :'sessions/new'
+  end
+end
+
+get '/sessions/logout' do
+  session.clear
+  redirect '/'
+end
