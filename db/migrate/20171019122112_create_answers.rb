a4c6faa9668691cc33fc45bae4a324d35125eaee
Chris Camp <cpcamp2@gmail.com>
2017-10-20 15:51:53 -0400

    migration update git push

--- db/migrate/20171019122112_create_answers.rb
+++ db/migrate/20171019122112_create_answers.rb
@@ -3,8 +3,8 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   	create_table :answers do |t|
       t.string :content, presence: true
       # t.boolean :best_answer, default: false
-      t.references :user_id, :null => false
-      t.references :question_id, :null => false
+      t.integer :user_id, :null => false
+      t.integer :question_id, :null => false
       t.timestamps
     end
   end
