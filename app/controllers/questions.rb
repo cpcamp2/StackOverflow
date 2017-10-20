0ba71cf89c287d93b63c34cf2452f54ac5e78ede
Chris Camp <cpcamp2@gmail.com>
2017-10-20 15:31:15 -0400

    Edit button for questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -14,14 +14,30 @@ post '/questions' do
   end
 end
 
-delete '/questions/:id' do
-  question = Question.find_by(id: params[:id])
-  question.destroy
-  redirect "/users/#{current_user.id}"
-end
-
 get '/question/:id' do
   @question = Question.find_by(id: params[:id])
   # @answers = @question.answers
   erb :'questions/show'
+end
+
+get '/question/:id/edit' do
+  @question = Question.find_by(id: params[:id])
+  if current_user == @question.user
+    erb :'questions/edit'
+  else
+    status 401
+    redirect "/questions/#{@question.id}"
+  end
+end
+
+patch '/question/:id' do
+  @question = Question.find_by(id: params[:id])
+  @question.update(params[:question])
+  redirect "/"
+end
+
+delete '/questions/:id' do
+  question = Question.find_by(id: params[:id])
+  question.destroy
+  redirect "/users/#{current_user.id}"
 end
